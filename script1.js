99c808f3a9802841858ff8c59caa2e250ff6238c
Umar Solanki <uysolanki@gmail.com>
2025-05-28 09:31:12 +0530

    products

--- script1.js
+++ script1.js
@@ -1,13 +1,45 @@
+// function changeHeading()
+// {
+//     const headingElements=document.getElementsByClassName("heading1");
+
+//     console.log(headingElements)
+//     //headingElements[0].textContent="Apple"
+
+//     Array.from(headingElements).forEach(
+//         (heading)=>{
+//                 heading.textContent="Apple"
+//         }
+//     )
+// }
+
+
+
+// function changeHeading()
+// {
+//     const headingElements=document.querySelectorAll('.heading1')
+
+//     console.log(headingElements)
+//     //headingElements[0].textContent="Apple"
+
+//     Array.from(headingElements).forEach(
+//         (heading,index)=>{
+//             if(index%2==1)
+//                 heading.textContent="Mango"
+//             else
+//                 heading.textContent="Banana"
+//         }
+//     )
+
+// }
+
+
 function changeHeading()
 {
-    const headingElements=document.getElementsByClassName("heading1");
-
-    console.log(headingElements)
-    //headingElements[0].textContent="Apple"
+    const headingElements=document.querySelectorAll('#amitabh div')
 
     Array.from(headingElements).forEach(
-        (heading)=>{
-                heading.textContent="Apple"
+        (divEle,index)=>{
+           divEle.style.backgroundColor="red"
         }
     )
 
