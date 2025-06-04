3fd6652d85951aefdef53c767f013bf0164d8684
Umar Solanki <uysolanki@gmail.com>
2025-06-04 15:32:15 +0530

    todo

---
+++ jssessions/todolist/todoscript2.js
@@ -0,0 +1,53 @@
+let todoList=['apple','mango']
+display()
+function addtolist() 
+{
+
+const textElement=document.getElementById('t1')
+const textData=textElement.value;
+const paraElement=document.getElementById("p1")
+//console.log(textData)
+if(todoList.includes(textData))
+{
+    //alert('Data allready exists')
+    //textElement.style.borderColor='red'
+    paraElement.style.display='block'
+
+}
+else
+{
+    paraElement.style.display='none'
+    todoList.push(textData)
+    textElement.value=""
+    textElement.focus();
+}
+
+display()
+}
+
+function display()
+{
+    const displayTodoDiv=document.getElementById('showtodo')
+    let displayList="";
+    todoList.forEach(
+    (entry,index)=>{
+       displayList+=`<div id='todo-row'><span>${entry}</span><button class='delete-button'>Delete</button></div>`
+    }
+)
+displayTodoDiv.innerHTML=displayList;
+
+const deleteButtons=document.querySelectorAll('.delete-button')
+console.log(deleteButtons)
+
+
+deleteButtons.forEach(
+    (singleDeleteButton,index)=>{
+        singleDeleteButton.addEventListener('click',()=>{
+            todoList.splice(index,1)
+            display()
+        });
+    });
+    
+}
+
+
