33eb8e07c76ca6465e098d4bd899a20366283dd1
Umar Solanki <uysolanki@gmail.com>
2025-06-06 14:45:42 +0530

    rps1

---
+++ jssessions/rps/rps1.js
@@ -0,0 +1,65 @@
+    let win=0;
+    let lose=0;
+    let tie=0;
+function playGame(playerMove)
+{
+    console.log(`Player Move is ${playerMove}`)
+    const computerMove=generateComputeMove();
+    console.log(`Computer Move is ${computerMove}`)
+    let result;
+    switch(playerMove)
+    {
+        case 'Rock':    
+                        switch(computerMove)
+                        {
+                            case 'Rock' : result='Tie';break;
+                            case 'Paper' : result='Lose';break;
+                            case 'Scissor' : result='Win';break;
+
+                        }
+        break;
+        case 'Paper':    
+                        switch(computerMove)
+                        {
+                            case 'Rock' : result='Win';break;
+                            case 'Paper' : result='Tie';break;
+                            case 'Scissor' : result='Lose';break;
+
+                        }
+        break;
+        case 'Scissor':    
+                        switch(computerMove)
+                        {
+                            case 'Rock' : result='Lose';break;
+                            case 'Paper' : result='Win';break;
+                            case 'Scissor' : result='Tie';break;
+
+                        }
+        break;
+    }
+
+    console.log(`Result : ${result}`)
+    
+
+    switch(result)
+    {
+        case "Win":win++; break;
+        case "Lose":lose++; break;
+        case "Tie":tie++; break;
+    }
+
+    console.log(`Score : Wins ${win} : Tie ${tie} : Loss ${lose}`)
+}
+
+
+function generateComputeMove()
+{
+    const randomNumber=Math.random();  //0->1
+    //console.log(randomNumber)
+    if(randomNumber<1/3)
+        return "Rock"
+    else if(randomNumber>1/3 && randomNumber<2/3)
+        return "Paper"
+    else
+        return "Scissor"
+}
