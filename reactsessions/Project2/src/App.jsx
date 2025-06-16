849156fd861f8b82d191e68e13ca528d269013a5
Umar Solanki <uysolanki@gmail.com>
2025-06-16 14:37:48 +0530

    children props

--- reactsessions/Project2/src/App.jsx
+++ reactsessions/Project2/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from './components/Button'
 import { useState } from 'react'
+import Header from './components/Header'
 
 const App = () => {
   const [flag,setFlag]=useState(true)
@@ -10,6 +11,10 @@ const App = () => {
   }
   return (
     <div>
+      <Header text="McDonalds">
+         
+ <p>I m Lovin It!!!</p>
+      </Header>
       <Button color={flag} handleClick={toggle} />
     </div>
   )
